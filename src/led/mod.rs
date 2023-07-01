8053b6c4b1bde9c776c7973b9103a29db252c102
Denis Isidoro <denis.isidoro@uber.com>
2023-07-01 07:34:35 -0300

    Jul-01 07h34: mod.rs, clap.rs

--- src/led/mod.rs
+++ src/led/mod.rs
@@ -5,9 +5,9 @@ pub mod command;
 mod payload;
 mod terminal;
 
-pub struct Device {
-    mac: String,
-    peripheral: Uuid,
-    service: Uuid,
-    characteristic: Uuid,
-}
+// pub struct Device {
+//     mac: String,
+//     peripheral: Uuid,
+//     service: Uuid,
+//     characteristic: Uuid,
+// }
